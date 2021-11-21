1e414416f56444e07e1ec10efe9818bcc9228811
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2021-11-22 00:07:22 +0100

    All the variables and functions added

--- src/main/scala/funkycompiler/Main.scala
+++ src/main/scala/funkycompiler/Main.scala
@@ -3,14 +3,6 @@ package funkycompiler
 import stage3.{ Tree, Variable, mkVarDefs }
 import stage4.mkXml
 
-// inline def ifStatement: Tree =
-//   val a = Variable("a")
-//   val b = Variable("b")
-//   if b > 10 then a := 20 else a := 10
-
-// @main def main =
-//   println(mkXml(mkVarDefs(stage {
-//   })))
 
 @main def main =
   val testPlane = java.io.File("/Users/kmetiuk/Library/Application Support/unity.Jundroo.SimplePlanes/AircraftDesigns/Test Plane.xml")
