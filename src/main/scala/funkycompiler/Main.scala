1e0dfd1a0d33b3de71d8be3baa3568f6ebb0a650
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2021-11-21 17:49:02 +0100

    Stage 4 compiler implemented

--- src/main/scala/funkycompiler/Main.scala
+++ src/main/scala/funkycompiler/Main.scala
@@ -1,19 +1,20 @@
 package funkycompiler
 
 import stage3.{ Tree, Variable, mkVarDefs }
+import stage4.mkXml
 
-inline def ifStatement: Tree =
-  val a = Variable("a")
-  val b = Variable("b")
-  if b > 10 then a := 20 else a := 10
+// inline def ifStatement: Tree =
+//   val a = Variable("a")
+//   val b = Variable("b")
+//   if b > 10 then a := 20 else a := 10
 
-@main def main = println(stage(ifStatement))
-  // println(mkVarDefs(stage {
-  //   val x = Variable("x")
-  //   x := { if x === 30 then 10 else 20 }
-  //   if x === 30 then
-  //     x := 40
-  //   else
-  //     x := x
-  //   50
-  // }))
+@main def main =
+  println(mkXml(mkVarDefs(stage {
+    val x = Variable("x")
+    x := { if x === 30 then 10 else 20 }
+    if x === 30 then
+      x := 40
+    else
+      x := x
+    50
+  })))
--- src/main/scala/funkycompiler/Stage4.scala
+++ src/main/scala/funkycompiler/Stage4.scala
@@ -12,3 +12,23 @@ object stage4:
   case class UnaryOp(rhs: Expr, sign: String) extends Expr
   case class If(condition: Expr, lhs: Expr, rhs: Expr | Null) extends Expr
   case class Call(name: String, args: List[Expr]) extends Expr
+
+  def mkXml(t: Tree): String = t match
+    case VarDefs(defs) =>
+      s"<Variables>\n${defs.map(mkXml).mkString("\n")}\n</Variables>"
+
+    case VarDef(name, expr, condition) =>
+      val maybeActivator = if condition != null then s"""activator="${mkXml(condition)}" """ else ""
+      s"""  <Setter name="$name" value="${mkXml(expr)}" $maybeActivator/>"""
+
+    case Const(x) => x.toString
+    case VarRef(name) => name
+    case BinaryOp(lhs, rhs, sign) => s"(${mkXml(lhs)})$sign(${mkXml(rhs)})"
+    case UnaryOp(rhs, sign) => s"${sign}(${mkXml(rhs)})"
+    case If(cnd, lhs, rhs) =>
+      if rhs == null then
+        throw RuntimeException("If you are using `if` to compute a value, " +
+          "it must have both `then` and `else` branches")
+      s"(${mkXml(cnd)})?(${mkXml(lhs)}):(${mkXml(rhs)})"
+
+    case Call(name, args) => s"$name(${args.map(mkXml).mkString(",")})"
