06e9a620fc1a046697302a64c3a566b5f1241b25
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2021-11-22 01:19:41 +0100

    Example program

--- src/main/scala/funkycompiler/Main.scala
+++ src/main/scala/funkycompiler/Main.scala
@@ -2,15 +2,40 @@ package funkycompiler
 
 import stage3.{ Tree, Variable, mkVarDefs }
 import stage4.mkXml
+import stdlib.*
 
 
 @main def main =
   val testPlane = java.io.File("/Users/kmetiuk/Library/Application Support/unity.Jundroo.SimplePlanes/AircraftDesigns/Test Plane.xml")
   val program = mkXml(mkVarDefs(stage {
-    val x = Variable("x")
-    if x < 500 then
-      x := x + 1
-    else
-      x := 0
+    val elevators = Variable("elevators")
+    val ailerons = Variable("ailerons")
+    val rudder = Variable("rudder")
+    val thrust = Variable("thrust")
+
+    val elevatorsStep = 0.001
+    val targetRateOfDescent = -6
+    val maxAllowedPitch = 0
+    val maxAllowedRateOfDescent = -25
+    val cutEnginesAltitude = 3
+
+    ailerons := Roll
+    rudder := Yaw
+
+    if Time === 0 then
+      elevators := 0
+
+
+    if PitchAngle < maxAllowedPitch then
+      elevators := elevators + elevatorsStep
+    else if PitchAngle > 5 & elevators > 0 then
+      elevators := elevators - elevatorsStep
+    elevators := clamp01(elevators + Pitch)
+
+    if AltitudeAgl < cutEnginesAltitude then thrust := 0
+    else thrust := (
+      if rate(AltitudeAgl) > targetRateOfDescent then 0.08
+      else if rate(AltitudeAgl) < maxAllowedRateOfDescent then 0.25
+      else 0.12)
   }))
   writeVariables(testPlane, program)
