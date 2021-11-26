f19c669681aa3136b38455bffa20b9b3925b2a38
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2021-11-26 15:02:04 +0100

    Test suite simplified

--- src/test/scala/funkycompiler/FunkySuite.scala
+++ src/test/scala/funkycompiler/FunkySuite.scala
@@ -4,12 +4,16 @@ import utest._
 import testPrograms.*
 
 
-object Stage4Suite extends TestSuite:
+object FunkySuite extends TestSuite:
   def testStr(str: String) = str.stripMargin.drop(1)
 
   val tests = Tests {
-    test("if statement"){
-      stage4.mkXml(stage3.mkVarDefs(stage(ifStatement))).toString ==> testStr("""
+    test("if statement") {
+      funky {
+        val a = Variable("a")
+        val b = Variable("b")
+        if b > 10 then a := 20 else a := 10
+      }.compile ==> testStr("""
         |<Variables>
         |  <Setter variable="a" function="20.0" activator="(b &gt; 10.0)" />
         |  <Setter variable="a" function="10.0" activator="(!(b &gt; 10.0))" />
@@ -17,7 +21,24 @@ object Stage4Suite extends TestSuite:
     }
 
     test("nested if statement") {
-      stage4.mkXml(stage3.mkVarDefs(stage(nestedIfStatement))).toString ==> testStr("""
+      funky {
+        val Flaps = Variable("Flaps")
+        val Autotrim = Variable("Autotrim")
+
+        val maxPitchRate = 0.01
+        object flaps:
+          val extensionRate = 0.02
+          val maxExtension = 30
+
+        if PitchRate > maxPitchRate then
+          if Flaps < flaps.maxExtension then
+            Flaps := Flaps + flaps.extensionRate
+          else
+            Autotrim := Autotrim + flaps.extensionRate
+        else if PitchRate < -maxPitchRate then
+          Flaps := Flaps - flaps.extensionRate
+        else Flaps := Flaps
+      }.compile ==> testStr("""
         |<Variables>
         |  <Setter variable="Flaps" function="(Flaps + 0.02)" activator="((PitchRate &gt; 0.01) &amp; (Flaps &lt; 30.0))" />
         |  <Setter variable="Autotrim" function="(Autotrim + 0.02)" activator="((PitchRate &gt; 0.01) &amp; (!(Flaps &lt; 30.0)))" />
@@ -27,7 +48,16 @@ object Stage4Suite extends TestSuite:
     }
 
     test("blocks should accumulate assignments and if statements") {
-      stage4.mkXml(stage3.mkVarDefs(stage(blocksAccumulation))).toString ==> testStr("""
+      funky {
+        val x = Variable("x")
+        val y = Variable("y")
+        x := 10
+        y := 20
+        x := 30
+        if x === 30 then
+          x := 40
+        50
+      }.compile ==> testStr("""
         |<Variables>
         |  <Setter variable="x" function="10.0" />
         |  <Setter variable="y" function="20.0" />
@@ -37,14 +67,21 @@ object Stage4Suite extends TestSuite:
     }
 
     test("calls") {
-      stage4.mkXml(stage3.mkVarDefs(stage(calls))).toString ==> testStr("""
+      funky {
+        val x = Variable("x")
+        x := abs(25)
+      }.compile ==> testStr("""
         |<Variables>
         |  <Setter variable="x" function="abs(25.0)" />
         |</Variables>""")
     }
 
     test("loop unrolling") {
-      stage4.mkXml(stage3.mkVarDefs(stage(loopUnrolling))).toString ==> testStr("""
+      funky {
+        val x = Variable("x")
+        for i <- 1 to 5 yield
+          x := i
+      }.compile ==> testStr("""
         |<Variables>
         |  <Setter variable="x" function="1.0" />
         |  <Setter variable="x" function="2.0" />
@@ -55,7 +92,21 @@ object Stage4Suite extends TestSuite:
     }
 
     test("boolean function") {
-      stage4.mkXml(stage3.mkVarDefs(stage(booleanFunction))).toString ==> testStr("""
+      def f(x: Tree): Tree = funky {
+        if x === 0 then
+          Pitch := 10
+          false
+        else
+          Pitch := 20
+          true
+      }
+
+      funky {
+        if f(10) then
+          Roll := 1
+        else
+          Roll := 20
+      }.compile ==> testStr("""
         |<Variables>
         |  <Setter variable="Pitch" function="10.0" activator="(10.0 = 0.0)" />
         |  <Setter variable="Pitch" function="20.0" activator="(!(10.0 = 0.0))" />
