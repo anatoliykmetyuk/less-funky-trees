e4b6720c48ddac616d3b18462ddfd6354af5c19e
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2021-12-09 13:22:07 +0100

    Coordinates fix

--- examples/src/main/scala/funkycompiler/planes/Pidgin.scala
+++ examples/src/main/scala/funkycompiler/planes/Pidgin.scala
@@ -20,40 +20,55 @@ object Pidgin:
   def bank(tgt: Tree) = funky {
     val delta = freshVar()
     val direction = freshVar()
+    val marker = Variable("bank_marker")
 
+    marker := 0
     while
       delta := deltaangle(RollAngle, tgt)
       direction := sign(delta)
       abs(delta) > 1 || abs(RollRate) > 10
     do
+      marker :+= 1
       ailerons := (
         if abs(delta) > 40 then 1 * direction
         else if abs(delta) > 1 then 0.1 * direction
         else 0)
+      marker :+= 1
   }
-
-  // def turn(direction: Tree) = bank(fastTurnAngle * sign(direction))
-  // def slowTurn(direction: Tree) = bank(slowTurnAngle * sign(direction))
-  // def levelWings = bank(0)
 
   def setHeading(hdg: Tree) = funky {
     val delta = freshVar()
     val direction = freshVar()
+    val marker = Variable("marker")
+    val marker2 = Variable("marker2")
+    val bank_marker = Variable("bank_marker")
 
+    marker := 0
     while
       delta := deltaangle(hdg, Heading)
       direction := sign(delta)
       abs(delta) > 1 || abs(rate(Heading)) > 10
     do
-      if abs(delta) > 10 then bank(direction * 80)
-      else if abs(delta) > 1 then bank(direction * 10)
+      marker :+= 1
+      if abs(delta) > 5 then bank(direction * 80)
+      else if abs(delta) > 1 then bank(direction * 40)
+      marker :+= 1
+    marker2 := 1
     bank(0)
+    marker2 :+= 1
   }
 
-  // def takeOff: Tree = funky {
-  //   thrust := 1
-  //   while Altitude < 300 do
-  //     if TAS > 50 then updatePitch(30)
-  // }
-
-
+  def goToCoordinates(lat: Tree, lon: Tree) = funky {
+    val distance = Variable("distance")
+    val dLat = Variable("dLat")
+    val dLon = Variable("dLon")
+    val hdg = Variable("coordHeading")
+    while
+      dLat := lat - Latitude
+      dLon := lon - Longitude
+      distance := sqrt(dLat*dLat + dLon*dLon)
+      hdg := (if dLat >= 0 then asin(dLon / distance) else 180 - asin(dLon / distance))
+      distance > 500
+    do
+      setHeading(hdg)
+  }
