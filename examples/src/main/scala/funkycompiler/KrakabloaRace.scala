56580aa9632d8b26a5a1a7f612aae2c701747fe4
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2021-12-09 18:04:07 +0100

    Krakabloa Racing works

--- examples/src/main/scala/funkycompiler/KrakabloaRace.scala
+++ examples/src/main/scala/funkycompiler/KrakabloaRace.scala
@@ -5,9 +5,27 @@ import stage3.*
 import stdlib.*
 import planes.Pidgin.{ file => pidgin, * }
 
+val flightpath: List[(Double, Double)] = List(
+  (57514, 18117),
+  (55786, 16899),
+  (53461, 16991),
+  (52678, 17726),
+  (52442, 21872),
+  (53521, 22993),
+  (54681, 21772),
+  (55473, 23612),
+  (55989, 22328),
+  (56800, 21871), // (56619, 21871),
+  (56581, 19000),
+)
 
 @main def KrakabloaRace = program(pidgin) {
   thrust := 1
-  goToCoordinates(57514.37, 18117.53)
-  goToCoordinates(55786, 16899)
+  for case ((lat, lon), id) <- flightpath.zipWithIndex yield
+    Variable("waypoint") := id
+    goToCoordinates(lat, lon)
+  while Altitude < 1400 do
+    elevators := PID(30, PitchAngle, 0.1, 0, 0.05)
+  while true do
+    elevators := PID(0, PitchAngle, 0.1, 0, 0.05)
 }
--- examples/src/main/scala/funkycompiler/planes/Pidgin.scala
+++ examples/src/main/scala/funkycompiler/planes/Pidgin.scala
@@ -15,17 +15,20 @@ object Pidgin:
   }
 
   def maintainBank(bnk: Tree) = funky {
-    while true do ailerons := deltaangle(RollAngle, bnk) / 40
+    while true do ailerons := deltaangle(RollAngle, bnk) / 60
   }
 
   def maintainHeading(hdg: Tree) = funky {
     val bnk = Variable("bnk")
     val delta = deltaangle(hdg, Heading)
+    val elevatorsError = -PitchAngle-smooth(AngleOfAttack, 1)
     maintainBank(bnk) & funky {
       while true do
-        bnk := clamp(delta / 20, -1, 1) * 80
-        elevators := PID(0,PitchAngle+smooth(AngleOfAttack, 0.1),
-          0.1,0,0.05)
+        bnk := clamp(delta / 10, -1, 1) * 85
+        elevators := 0.1*elevatorsError + 0.05*rate(elevatorsError)
+
+          // PID(0,PitchAngle+smooth(AngleOfAttack, 0.1),
+          // 0.1,0,0.05)
         // if abs(abs(RollAngle) - 80) < 2 && abs(delta) > 30 then elevators := 1
     }
   }
